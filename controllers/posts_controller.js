6f5cf6e25ca370d73841433886559590990f4eba
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-10-03 20:30:21 +0530

    Added authorized delete post functionality along with auto delete related comments

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,6 @@
+const { request } = require('express');
 const Post = require('../models/posts');
+const Comment = require('../models/comments');
 
 module.exports.createPost = function(req, res){
     Post.create({
@@ -13,5 +15,27 @@ module.exports.createPost = function(req, res){
         console.log("Post created successfully : ", newPost);
         // return;
         return res.redirect('back');
+    })
+}
+
+module.exports.destroy = function(req, res){
+
+    Post.findById(req.params.id, function(err, post){
+
+        // .id means it is converting the _id into string 
+        if(post.user == req.user.id){
+            post.remove();
+            Comment.deleteMany({ post : req.params.id }, function(err){
+                if(err){
+                    console.log('Error deleting post : ', err );
+                    return;
+                }
+            });
+            console.log('Post deleted successfully : ', post);
+            return res.redirect('back'); 
+        }
+        else{
+            return res.redirect('back'); 
+        }
     })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,6 @@ const passport = require('passport');
 const postsController = require('../controllers/posts_controller');
 
 router.post('/create', passport.checkAuthentication ,postsController.createPost);
+router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy);
 
 module.exports = router;

c9ec66aa4f17d14c97c8bb77733d1b76862dac35
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-10-03 22:37:26 +0530

    Added functionality that both post owner and commenter can delete the comments on related post

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -25,3 +25,85 @@ module.exports.create = function(req, res){
     })
 
 }
+
+
+module.exports.destroy = function(req, res){
+    Comment.findById(req.params.id, function(err, comment){
+        if(err){
+            console.log('Error in finding comment : ', err);
+            return;
+        }
+
+
+        console.log('Current user Id : ', req.user.id);
+
+
+        let postIdforOwner = comment.post;
+        console.log('Post Id : ',postIdforOwner);
+        let postOwner;
+
+        Post.findById(postIdforOwner, function(err, post){
+            if(err){
+                return;
+            }
+            postOwner = post.user;
+            console.log('Post owner : ',postOwner);
+        });
+
+        console.log('Commenter Id : ', comment.user);
+
+        //the comment should be deleted by both the post owner and commenter
+        if(comment.user == req.user.id){
+            let postId = comment.post;
+            comment.remove();
+            console.log('Removing the comment by commenter..')
+            //$pull is built in function to pull out things from db
+            Post.findByIdAndUpdate(postId,{ $pull : {comments : req.params.id}}, function(err, post){
+                return res.redirect('back');
+            })
+        }
+        else{
+            console.log('Failed :: Removing the comment by commenter..')
+            return res.redirect('back');
+        }
+    });
+}
+module.exports.postownerDestroy = function(req, res){
+    Comment.findById(req.params.id, function(err, comment){
+        if(err){
+            console.log('Error in finding comment : ', err);
+            return;
+        }
+
+
+        console.log('Current user Id : ', req.user.id);
+
+
+        let postIdforOwner = comment.post;
+        console.log('Post Id : ',postIdforOwner);
+
+        Post.findById(postIdforOwner, function(err, post){
+            if(err){
+                return;
+            }
+            let postOwner = post.user;
+            console.log('Post owner : ',postOwner);
+            //the comment should be deleted by both the post owner and commenter
+            if(postOwner == req.user.id){
+                let postId = comment.post;
+                comment.remove();
+                console.log('Removing the comment by post owner..')
+                //$pull is built in function to pull out things from db
+                Post.findByIdAndUpdate(postId,{ $pull : {comments : req.params.id}}, function(err, post){
+                    return res.redirect('back');
+                })
+            }
+            else{
+                console.log('Failed :: Removing the comment by owner..')
+                return res.redirect('back');
+            }
+        });
+
+        console.log('Commenter Id : ', comment.user);
+    });
+}
--- routes/comments.js
+++ routes/comments.js
@@ -4,6 +4,8 @@ const passport = require('passport');
 
 const commentsController = require('../controllers/comments_controller');
 
-router.post('/create', passport.checkAuthentication , commentsController.create);
+router.post('/create', passport.checkAuthentication, commentsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
+router.get('/postownerDestroy/:id', passport.checkAuthentication, commentsController.postownerDestroy);
 
 module.exports = router;

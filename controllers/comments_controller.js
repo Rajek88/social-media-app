d347cd8a6d2a5516966e125e8dacaec80788dfd7
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-10-05 18:55:20 +0530

    added async await in code wherever needed

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,56 +1,37 @@
 const Comment = require('../models/comments');
 const Post = require('../models/posts');
 
-module.exports.create = function(req, res){
+module.exports.create = async function(req, res){
 
-    Post.findById(req.body.post, function(err, post){
-        if(err){
-            console.log('Error in fetching post by id :: ', err);
-            return;
-        }
+    try {
+        let post = await Post.findById(req.body.post);
 
         if(post){
-            Comment.create({
+            let comment = await Comment.create({
                 content : req.body.content,
                 post : req.body.post,
                 user : req.user._id,
-            }, function(err, comment){
-                //handle error
-                post.comments.push(comment);
-                post.save();
-                return res.redirect('/');
             });
+
+            post.comments.push(comment);
+            post.save();
+            return res.redirect('/');
         }
 
-    })
+    } catch (error) {
+        console.log('Error in creating comment : ', error);
+        return;
+    }
 
 }
 
 
-module.exports.destroy = function(req, res){
-    Comment.findById(req.params.id, function(err, comment){
-        if(err){
-            console.log('Error in finding comment : ', err);
-            return;
-        }
-
+module.exports.destroy = async function(req, res){
+    try {
+        let comment = await Comment.findById(req.params.id);
 
         console.log('Current user Id : ', req.user.id);
 
-
-        let postIdforOwner = comment.post;
-        console.log('Post Id : ',postIdforOwner);
-        let postOwner;
-
-        Post.findById(postIdforOwner, function(err, post){
-            if(err){
-                return;
-            }
-            postOwner = post.user;
-            console.log('Post owner : ',postOwner);
-        });
-
-        console.log('Commenter Id : ', comment.user);
 
         //the comment should be deleted by both the post owner and commenter
         if(comment.user == req.user.id){
@@ -66,44 +47,36 @@ module.exports.destroy = function(req, res){
             console.log('Failed :: Removing the comment by commenter..')
             return res.redirect('back');
         }
-    });
+    } catch (error) {
+        console.log('Error deleting comment : ', error);
+        return;
+    }
 }
-module.exports.postownerDestroy = function(req, res){
-    Comment.findById(req.params.id, function(err, comment){
-        if(err){
-            console.log('Error in finding comment : ', err);
-            return;
-        }
-
-
+module.exports.postownerDestroy = async function(req, res){
+    try {
+        let comment = await Comment.findById(req.params.id);
         console.log('Current user Id : ', req.user.id);
-
-
         let postIdforOwner = comment.post;
         console.log('Post Id : ',postIdforOwner);
 
-        Post.findById(postIdforOwner, function(err, post){
-            if(err){
-                return;
-            }
-            let postOwner = post.user;
-            console.log('Post owner : ',postOwner);
-            //the comment should be deleted by both the post owner and commenter
-            if(postOwner == req.user.id){
-                let postId = comment.post;
-                comment.remove();
-                console.log('Removing the comment by post owner..')
-                //$pull is built in function to pull out things from db
-                Post.findByIdAndUpdate(postId,{ $pull : {comments : req.params.id}}, function(err, post){
-                    return res.redirect('back');
-                })
-            }
-            else{
-                console.log('Failed :: Removing the comment by owner..')
+        let post = await Post.findById(postIdforOwner);
+        let postOwner = post.user;
+        console.log('Post owner : ',postOwner);
+        //the comment should be deleted by both the post owner and commenter
+        if(postOwner == req.user.id){
+            let postId = comment.post;
+            comment.remove();
+            console.log('Removing the comment by post owner..')
+            //$pull is built in function to pull out things from db
+            Post.findByIdAndUpdate(postId,{ $pull : {comments : req.params.id}}, function(err, post){
                 return res.redirect('back');
-            }
-        });
-
-        console.log('Commenter Id : ', comment.user);
-    });
+            })
+        }
+        else{
+            console.log('Failed :: Removing the comment by owner..')
+            return res.redirect('back');
+        }
+    } catch (error) {
+        console.log('Error deleting comment by post owner : ', error);
+    }
 }

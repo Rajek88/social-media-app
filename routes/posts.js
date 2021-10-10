6e3cd82872a2bb02d955d59d002a669235670007
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-10-10 15:04:44 +0530

    Created total post image system with models and post content validation and stuff, took 5 hours Bug fixes

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const expressValidator = require('express-validator')
 
 const postsController = require('../controllers/posts_controller');
 
